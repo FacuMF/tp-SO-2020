5789890be1ce4d7f53b9422da6e71db46814cedf
adelariestra <adelariestra@gmail.com>
2020-05-01 18:07:51 -0300

    resolucion conflictos servidor.c

--- Servidor/src/servidor.c
+++ Servidor/src/servidor.c
@@ -13,13 +13,9 @@ int main(void)
 	char * ip ;
 	char * puerto ;
 
-<<<<<<< HEAD
-	logger = malloc(sizeof(t_log));
+
 
 	logger = iniciar_logger("./servidor.log","Server");
-=======
-	logger = iniciar_logger();
->>>>>>> fa208c14d07e0bd5053d26559bf37eb7ca2c860f
 
 	config = leer_config();
 
@@ -31,26 +27,6 @@ int main(void)
 	finalizar_ejecucion(logger,config);
 
 	return EXIT_SUCCESS;
-}
-
-<<<<<<< HEAD
-=======
-
-t_log* iniciar_logger(void)
-{
-	t_log * logger = malloc(sizeof(t_log));
-	return log_create("./servidor.log","Server",true,LOG_LEVEL_INFO);
-}
-
->>>>>>> fa208c14d07e0bd5053d26559bf37eb7ca2c860f
-t_config*leer_config(){
-	t_config *config;
-	config = config_create("/home/utnso/Documentos/tp-2020-1c-PokEbola/configGeneral.config");
-		if(config ==NULL){
-			printf("Error en config.\n");
-			exit(3);
-		}
-		return config;
 }
 
 void finalizar_ejecucion(t_log* logger,t_config *config){
