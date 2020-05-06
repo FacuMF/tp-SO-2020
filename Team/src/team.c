ca66a57623591e3250381907ef418278b7f68cee
francogonzalezorellano <fgonzalezorellano@est.frba.utn.edu.ar>
2020-05-06 14:26:22 -0300

    Agrego logger y leo config en Teams

--- Team/src/team.c
+++ Team/src/team.c
@@ -4,16 +4,34 @@ int counter;
 pthread_mutex_t lock;
 
 int main(void){
+	// ** INICIALIZACION **
+
 	int i;
-	int cantidad_entrenadores;
-	t_config* config;
-	config = config =leer_config("/team.config");
-	//entrenador * head_entrenadores = list_create();
-	//char ** posiciones,pokemones_capturados,objetivos;
-	// Obtener de config los arrays y cargar las caracteristicas de los entrenadores a mi head
-	char ** posiciones = config_get_array_value(config,"POSICION_ENTRENADORES");
+//	int cantidad_entrenadores;
+
+	// Iniciar Logger
+	logger = iniciar_logger("./Team/config/team.log","Team");
+	log_info(logger,"Primer log ingresado");
+
+	//		** Obtener los entrenadores y localizarlos **
+
+	config =leer_config("./Team/config/team.config");
+	log_info(logger,"Config creada");
+	t_list * head_entrenadores = list_create();
+
+	char ** posiciones = config_get_array_value(config,"POSICIONES_ENTRENADORES");
+	log_info(logger, "Leido de config, posiciones: %s", config_get_string_value(config, "POSICIONES_ENTRENADORES"));
 	char ** pokemones_capturados = config_get_array_value(config,"POKEMON_ENTRENADORES");
+	log_info(logger, "Leido de config, pokemones capturados: %s", config_get_string_value(config, "POKEMON_ENTRENADORES"));
 	char ** objetivos = config_get_array_value(config,"OBJETIVOS_ENTRENADORES");
+	log_info(logger, "Leido de config, pokemones por capturar: %s", config_get_string_value(config, "OBJETIVOS_ENTRENADORES"));
+
+	cargar_y_localizar_entrenadores(head_entrenadores, posiciones, pokemones_capturados, objetivos);
+
+
+
+
+	/*
 	char entrenadores[3][50];
 	for(i=0;i<3;i++){
 			printf("La palabra es: %s",entrenadores[i]);
@@ -41,14 +59,37 @@ int main(void){
 	    pthread_mutex_destroy(&lock);
 	    }
 	    */
+
+	terminar_logger(logger);
+	config_destroy(config);
+
+
 }
 
+void cargar_y_localizar_entrenadores(t_list* head_entrenadores, char** posiciones, char** pokemones_capturados,char** objetivos){
+	entrenador* aux;
+	int contador;
+
+	for(contador=0, contador<=10, contador++){
+		aux->posicion = de_string_a_posicion(posiciones[contador]);
+		aux->pokemones_capturados = pokemones_capturados[contador];
+		aux->pokemones_por_capturar = objetivos[contador];
+
+		list_add(head_entrenadores, aux)
+
+	}
+
+	free(aux);
+}
+
+
+
+/*
 void printoutarray(char ** pointeratoarray){
 	int i,j;
 	for( i=0;i<=2;i++){
 		for(j=0;j<=2;j++){
 			printf("\n Element is: %s \n",&pointeratoarray[i]);
-
 	}
 }
 
@@ -62,7 +103,7 @@ void* doSomeThing(void *arg){
     for(i=0; i<(0xFFFFFFFF);i++);
     printf("\n Hilo %d finished\n", counter);
     pthread_mutex_unlock(&lock);
-    return NULL;
 }
+*/
 
 
--- Team/src/team.h
+++ Team/src/team.h
@@ -13,19 +13,24 @@
 #include<pthread.h>
 #include<unistd.h>
 
+#include "../../Base/Utils/src/utils.h"
+#include "../../Base/Utils/src/utils_mensajes.h"
+
+
+t_log* logger;
+t_config* config;
+
 
 
 typedef struct pokemones {
 	char * pokemon;
-    struct pokemones * next;
 }pokemones;
 
 
 typedef struct node {
     int posicion[2];
-    pokemones pokemonesObtenido;
-    pokemones pokemonesPorObtener;
-    struct node * next;
+    pokemones pokemones_capturados;
+    pokemones pokemones_por_capturar;
 } entrenador;
 
 
@@ -34,6 +39,7 @@ void printoutarray(char ** pointeratoarray);
 void* doSomeThing(void *arg);
 void iniciarListaEntrenador(entrenador *);
 int length(entrenador *);
+void cargar_y_localizar_entrenadores(t_list* head_entrenadores, char** posiciones, char** pokemones_capturados,char** objetivos);
 
 
 #endif /* TEAM_H_ */
