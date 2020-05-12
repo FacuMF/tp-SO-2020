ddcd6b1a0ebc687cd8dff7941855da97f1b8d9a6
francogonzalezorellano <fgonzalezorellano@est.frba.utn.edu.ar>
2020-05-12 13:28:31 -0300

    Inicializo colas de mensajes

--- Broker/src/broker.c
+++ Broker/src/broker.c
@@ -9,6 +9,83 @@ int main(void) {
 	config =leer_config("./Broker/config/broker.config");
 	log_info(logger,"Config creada");
 
+	// Inicializacion de las distintas colas de mensajes
+
+		//  NEW_POKEMON
+	t_queue* new_pokemon = malloc(sizeof(t_queue));
+	new_pokemon->subscriptores = list_create();
+	new_pokemon->mensajes = list_create();
+		//  APPEARED_POKEMON
+	t_queue* appeared_pokemon = malloc(sizeof(t_queue));
+	new_pokemon->subscriptores = list_create();
+	new_pokemon->mensajes = list_create();
+		//  CATCH_POKEMON
+	t_queue* catch_pokemon = malloc(sizeof(t_queue));
+	new_pokemon->subscriptores = list_create();
+	new_pokemon->mensajes = list_create();
+		//  CAUGHT_POKEMON
+	t_queue* caught_pokemon = malloc(sizeof(t_queue));
+	new_pokemon->subscriptores = list_create();
+	new_pokemon->mensajes = list_create();
+		//  GET_POKEMON
+	t_queue* get_pokemon = malloc(sizeof(t_queue));
+	new_pokemon->subscriptores = list_create();
+	new_pokemon->mensajes = list_create();
+		//  LOCALIZED_POKEMON
+	t_queue* localizad_pokemon = malloc(sizeof(t_queue));
+	new_pokemon->subscriptores = list_create();
+	new_pokemon->mensajes = list_create();
+
+
+
+	//  Administrar Subsciptores
+
+		// Espera solicitudes de distintos modulos.
+
+		// Lista de subscriptores por cada cola que administra.
+
+		// Enviar a nuevos subscriptores, los mensajes cacheados
+
+
+
+	//  Administrar recepcion, envio y confirmacion de mensajes
+
+		//  Recibir mensajes
+
+			//  Analizar a que cola pertenece.
+
+			//  Identificar unoquibocamente el mensaje (ID)
+
+			//  Almacenar en dicha cola.
+
+			//  Cachear mensajes
+
+	//  Enviar a todos los subscriptores
+
+			//  Todo mensaje debe permanecer en la cola hasta que todos sus
+			//  subs lo reciban
+
+			//  Notificacion de recepcion: Todo mnesaje debe ser confirmado
+			//  por cada subscriptor, para no volver a enviarlo al mismo.
+
+			//  La recepcion y notificacion de mensajes puede diferir en
+			//  el tiempo
+
+
+
+	//  Mantener un registro de los ultimos mensajes recibidos para
+	//  futuros subs
+
+	//  Mantener e infomrar en todo momento los estados de las colas
+	//  con sus mensajes y subscriptores.
+
+		//  Mantener su estado
+
+		//  Borrar mensajes que fueron entregados a todos los subs.
+
+
+
+
 
 
 
--- Broker/src/broker.h
+++ Broker/src/broker.h
@@ -3,3 +3,16 @@
 
 t_log* logger;
 t_config* config;
+
+typedef struct{
+	t_list subscriptores;
+	t_list mensajes; // lista de t_mensajes
+}t_queue;
+
+typedef struct{
+	t_tipo_mensaje tipo_mensaje;
+	int id;
+	int id_correlacional;
+	t_list subscribers_enviados; //Subscriptores a esta cola
+	t_list subscribers_ack; //Subscriptores que recibieron el mensaje
+}t_mensaje;
