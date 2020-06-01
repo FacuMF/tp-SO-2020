2e626bd8735881a11d6f546f749c8cefdf4a7048
FedePalu <fedepalu87@gmail.com>
2020-06-01 07:52:21 -0300

    Agregando mas funciones de path y armando un poco mas la estructura del gamecard

--- Gamecard/src/gamecard.c
+++ Gamecard/src/gamecard.c
@@ -4,24 +4,32 @@ void main(){
 	// Leer config
 	config = leer_config("./Gamecard/config/gamecard.config");
 	set_base_path(config_get_string_value(config,"PUNTO_MONTAJE_TALLGRASS"));
-	int PUERTO_BROKER = config_get_int_value(config, "PUERTO_BROKER");
+	char* PUERTO_BROKER = config_get_int_value(config, "PUERTO_BROKER");
+	char* IP_BROKER = config_get_int_value(config, "IP_BROKER");
 	int REINTENTO_CONEXION = config_get_int_value(config, "TIEMPO_DE_REINTENTO_CONEXION");
 	int REINTENTO_OPERACION = config_get_int_value(config, "TIEMPO_DE_REINTENTO_OPERACION");
+	// Conexion socket
+	int conexion = iniciar_conexion(IP_BROKER, PUERTO_BROKER);
 
 	// Suscribirse al broker: NEW_POKEMON, CATCH_POKEMON, GET_POKEMON
-
-	// Esperar a recibir mensajes.
+	suscribirse_a(&conexion,"NEW_POKEMON" ); // NEW_POKEMON
+	suscribirse_a(&conexion,"CATCH_POKEMON" ); // CATCH_POKEMON
+	suscribirse_a(&conexion,"GET_POKEMON" ); // GET_POKEMON
+	// "NEW_POKEMON" "CAUGHT_POKEMON" "GET_POKEMON" deben ser int
+	// Esperar a recibir mensajes. Reintentar cada REINTENTO_CONEXION si no se logra conectar
+	lanzar_hilo_receptor_mensajes(); // Chequear función
 
 	// Al recibir: 	Informar la recepción del mismo (ACK)
 	//				Crear hilo que atienda solicitud
 	//				Volver al estado escucha de la MQ
 
-	// Todo archivo tiene un valor "OPEN" en su metadata
-	pthread_t thread_socket, thread_dump;
+	/*
+	pthread_t thread_socket;
 	pthread_create(&thread_socket, NULL, (void*) listen, PUERTO_BROKER);
+	*/
 
 	//Al recibir NEW_POKEMON
-
+	recibir_mensajes_gamecard();
 	if(file_open()){
 		// Reintentar la operación luego de REINTENTO_OPERACION
 	} else {
--- Gamecard/src/utils_gamecard.c
+++ Gamecard/src/utils_gamecard.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include "../../Base/Utils/src/utils.h"
+
 
 // CONSTANT
 char* PUNTO_MONTAJE;
@@ -26,7 +28,6 @@ void set_base_path(char* base) {
     PUNTO_MONTAJE = base;
 }
 
-// Metadata files?
 char* to_block_file(int blockNumber) {
     return concat(string_itoa(blockNumber), EXTENSION);
 }
@@ -41,11 +42,13 @@ char* blocks_base_path() {
     return concat(PUNTO_MONTAJE, BLOCKS_BASE_PATH);
 }
 
-char* files_base_path() {
-    return concat(PUNTO_MONTAJE, FILES_BASE_PATH);
+
+char* files_base_path(char* fileName) {
+    return concat_dirs(concat(PUNTO_MONTAJE, FILES_BASE_PATH), fileName);
 }
 
-// PATH DEFINITIVOS
+
+// ----- PATH DEFINITIVOS -----
 
 char* block_path(int block) {
     return concat(blocks_base_path(), to_block_file(block));
@@ -59,24 +62,15 @@ char* bitmap_path() {
     return concat(metadata_base_path(), BITMAP_FILE_NAME);
 }
 
-char* files_path() {
-    return concat(?? , ??); // VER
+char* pokemon_metadata_path(char* fileName) {
+    return concat(files_base_path(fileName), METADATA_FILE_NAME); // Se debería agregar un '/' al comienzo del METADATA_FILE_NAME
 }
 
-
-void listen(int port) {
-    printf("TODO: Listen %d\n", port);
-}
+// Manejo de archivos
 
 void create_file(char* path) {
-    FILE* file = fopen(path, "rb");
-    if(file == NULL){
-    	fopen(path, "wb");
-    } else {
-    	printf("El archivo ya existe"); //log?
-    }
-	fclose(file);
-
+    FILE* file = fopen(path, "w");
+    fclose(file);
 }
 
 void create_dir(char* path) {
@@ -117,4 +111,62 @@ void write_file(char* path, char* data) {
     fclose(file);
 }
 
+// Base de crear archivos
+
+t_config* read_pokemon_metadata(char* table) {
+    return config_create(pokemon_metadata_path(table));
+}
+
+void create_pokemon_dir(char* tableName) {
+    create_dir(files_base_path(tableName));
+}
+
+void create_pokemon_metadata_file(char* tableName){
+    create_file(pokemon_metadata_path(tableName));
+    t_config* config = read_pokemon_metadata(tableName);
+    config_set_value(config, "DIRECTORY", "N");
+    config_set_value(config, "SIZE", "0"); // A DEFINIR
+    config_set_value(config, "BLOCKS", "[]"); // A DEFINIR
+    config_set_value(config, "OPEN", "Y"); // A DEFINIR
+    config_save(config);
+    config_destroy(config);
+}
+
+
+// Crear archivos
+
+void create_new_file_pokemon(char* pokemon) {
+    create_pokemon_dir(pokemon);
+    create_pokemon_metadata_file(pokemon);
+}
+
+
+
+// Funciones generales
+
+void suscribirse_a(int* conexion, int cola){
+	t_subscriptor* mensaje = crear_suscripcion(cola,100); // tiempo? y cola ints?
+	t_buffer* buffer = serializar_suscripcion(mensaje);
+	enviar_mensaje(*conexion, buffer, 8); // 8 = op_code suscriptor
+
+}
+
+void recibir_mensajes_gamecard(int *socket){
+	 int cod_op = recibir_codigo_operacion(*socket);
+	 handle_mensajes_gamecard(cop_op, *socket);
+}
+
+
+void handle_mensajes_gamecard(int cod_op, int socket){
+	t_buffer * buffer;
+
+	switch(cod_op){
+	case NEW_POKEMON:
+		// logear new_pokemon recibido
+		buffer = recibir_mensaje(socket);
+		t_new_pokemon pokemon = deserializar_new_pokemon(buffer);
+
+	}
+
+}
 
